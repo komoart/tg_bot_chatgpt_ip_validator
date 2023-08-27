996b533a817d2de4c8a7eb02e36bfb849202234e
artem <crank2303@gmail.com>
2023-08-27 19:27:33 +0300

    added password

--- python_developer/2_task/2_task.py
+++ python_developer/2_task/2_task.py
@@ -2,20 +2,35 @@ import os
 
 import openai
 import telegram
+from telegram import Update, ForceReply
 from dotenv import load_dotenv
-from telegram.ext import ConversationHandler, Updater
+from telegram.ext import ConversationHandler, Updater, CommandHandler, MessageHandler, Filters, CallbackContext
 
 load_dotenv()
 
 
 TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-
+PASSWORD = "4321"
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 openai.api_key = OPENAI_API_KEY
 model_engine = "text-davinci-003"
 
+
+def start(update: Update, context: CallbackContext):
+    update.message.reply_text('This is a password-protected bot.\n\nPlease enter the password:',
+                              reply_markup=ForceReply())
+
+
+def password_check(update: Update, context: CallbackContext):
+    user_input = update.message.text
+
+    if user_input == PASSWORD:
+        update.message.reply_text('Access granted! Welcome to the bot.')
+    else:
+        update.message.reply_text('Access denied! Invalid password. Try again.',
+                                  reply_markup=ForceReply())
 
 def respond(update, context):
 
@@ -35,11 +50,14 @@ def respond(update, context):
 
 def main():
 
+
     updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
     dispatcher = updater.dispatcher
 
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(MessageHandler(Filters.reply, password_check))
 
-    dispatcher.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, respond))
+    dispatcher.add_handler(telegram.ext.MessageHandler(Filters.text & ~Filters.command, respond))
     updater.start_polling()
     updater.idle()
 
