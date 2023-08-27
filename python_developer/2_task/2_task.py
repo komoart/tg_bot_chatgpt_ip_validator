43ae3049357ea728ed9cf1d31f193f7547116f5d
artem <crank2303@gmail.com>
2023-08-27 21:47:46 +0300

    added db and lintering

--- python_developer/2_task/2_task.py
+++ python_developer/2_task/2_task.py
@@ -1,94 +1,89 @@
 import os
+import sqlite3
 
 import openai
-import telegram
-from telegram import Update, ForceReply
 from dotenv import load_dotenv
-from telegram.ext import ConversationHandler, Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-import sqlite3
+from telegram import Update, ForceReply
+from telegram.ext import CallbackContext, CommandHandler, Filters, MessageHandler, Updater
 
 load_dotenv()
 
 
-TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
-OPENAI_API_KEY = os.environ.get('OPENAI_API_KEY')
-PASSWORD = "4321"
-
-bot = telegram.Bot(token=TELEGRAM_TOKEN)
-openai.api_key = OPENAI_API_KEY
-model_engine = "text-davinci-003"
-
-
-def create_database():
-    connection = sqlite3.connect("message_history.db")
-    cursor = connection.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS messages (
-                        id INTEGER PRIMARY KEY,
-                        user_id INTEGER,
-                        username TEXT,
-                        message_id INTEGER,
-                        date TEXT,
-                        text TEXT)''')
-    connection.commit()
-    connection.close()
-
-create_database()
-
-def save_message(user_id, username, message_id, date, text):
-    connection = sqlite3.connect("message_history.db")
-    cursor = connection.cursor()
-    cursor.execute("INSERT INTO messages (user_id, username, message_id, date, text) VALUES (?, ?, ?, ?, ?)",
-                   (user_id, username, message_id, date, text))
-    connection.commit()
-    connection.close()
-
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text('Приветствую!\n\nВведите пароль:',
-                              reply_markup=ForceReply())
+class Settings:
+    telegram_token = os.environ.get('TELEGRAM_TOKEN')
+    openai_api_key = os.environ.get('OPENAI_API_KEY')
+    password = os.environ.get('PASSWORD')
+    model_engine = "text-davinci-003"
+    greeting_message = "Приветствую!\n\nВведите пароль:"
+    correct_pass_msg = "Пароль верный. Приятного пользования."
+    incorrect_pass_msg = "Пароль неверный. Попробуйте еще раз."
+    db_name = "message_history.db"
 
 
-def password_check(update: Update, context: CallbackContext):
-    user_input = update.message.text
+class DB:
+    def create_database(self):
+        connection = sqlite3.connect(Settings().db_name)
+        cursor = connection.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS messages (
+                            id INTEGER PRIMARY KEY,
+                            user_id INTEGER,
+                            username TEXT,
+                            message_id INTEGER,
+                            date TEXT,
+                            text TEXT)''')
+        connection.commit()
+        connection.close()
 
-    if user_input == PASSWORD:
-        update.message.reply_text('Пароль верный. Приятного пользования.')
-    else:
-        update.message.reply_text('Пароль неверный. Попробуйте еще раз.',
-                                  reply_markup=ForceReply())
+    def save_message(self, user_id, username, message_id, date, text):
+        connection = sqlite3.connect(Settings().db_name)
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO messages (user_id, username, message_id, date, text) VALUES (?, ?, ?, ?, ?)",
+                       (user_id, username, message_id, date, text))
+        connection.commit()
+        connection.close()
 
-def respond(update, context):
 
-    message_text = update.message.text
-    save_message(update.message.from_user.id, update.message.from_user.username, update.message.message_id, update.message.date, update.message.text)
+class Telegram:
+    def start(self, update: Update, context: CallbackContext):
+        update.message.reply_text(Settings().greeting_message, reply_markup=ForceReply())
 
-    response = openai.Completion.create(
-        model=model_engine,
-        prompt=message_text,
-        max_tokens=300,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
+    def password_check(self, update: Update, context: CallbackContext):
+        user_input = update.message.text
 
-    update.message.reply_text(response.choices[0].text)
-    # bot_username = bot_answer.from_user.username
-    # bot_user_id = bot_answer.from_user.id
-    # save_message(bot_user_id, bot_username, bot_answer.message_id, bot_answer.date, bot_answer)
-    save_message("Bot", "Bot", update.message.message_id,
-                 update.message.date, response.choices[0].text)
+        if user_input == Settings().password:
+            update.message.reply_text(Settings().correct_pass_msg)
+        else:
+            update.message.reply_text(Settings().incorrect_pass_msg, reply_markup=ForceReply())
+
+    def respond(self, update, context):
+        up_m = update.message
+        message_text = up_m.text
+        DB().save_message(up_m.from_user.id, up_m.from_user.username, up_m.message_id, up_m.date, up_m.text)
+
+        response = openai.Completion.create(
+            model=Settings().model_engine,
+            prompt=message_text,
+            max_tokens=300,
+            n=1,
+            stop=None,
+            temperature=0.7,
+        )
+
+        up_m.reply_text(response.choices[0].text)
+        DB().save_message("Bot", "Bot", up_m.message_id, up_m.date, response.choices[0].text)
+
 
 def main():
-
-
-    updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+    openai.api_key = Settings().openai_api_key
+    DB().create_database()
+    updater = Updater(token=Settings().telegram_token, use_context=True)
     dispatcher = updater.dispatcher
-
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(MessageHandler(Filters.reply, password_check))
-
-    dispatcher.add_handler(telegram.ext.MessageHandler(Filters.text & ~Filters.command, respond))
+    dispatcher.add_handler(CommandHandler('start', Telegram().start))
+    dispatcher.add_handler(MessageHandler(Filters.reply, Telegram().password_check))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, Telegram().respond))
     updater.start_polling()
     updater.idle()
+
 
 if __name__ == '__main__':
     main()
